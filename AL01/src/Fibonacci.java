5895780bd5fa1ac90fb969e18f5d3b0ba43eac99
Kaarel Kivistik <kaarelkivistik@gmail.com>
2015-09-07 18:49:18 +0300

    RoundingMode muudetud

--- AL01/src/Fibonacci.java
+++ AL01/src/Fibonacci.java
@@ -1,19 +1,17 @@
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.math.RoundingMode;
 
 public class Fibonacci {
 
     public static final BigDecimal ONE = new BigDecimal("1");
     public static final BigDecimal TWO = new BigDecimal("2");
     public static final BigDecimal SQRT_FIVE = new BigDecimal(Math.sqrt(5));
-    public static final BigDecimal GOLDEN_RATIO = ONE.add(SQRT_FIVE).divide(TWO);
 
     /** 
      * @param n 
      * @return Computes the n-th number in Fibonacci series
      */
-    public static BigInteger fib(int n) {
+    public static int fib(int n) {
         BigInteger x = new BigInteger("0");
         BigInteger y = new BigInteger("1");
         BigInteger z = new BigInteger("1");
@@ -25,11 +23,11 @@ public class Fibonacci {
             z = x.add(y);
         }
 
-        return x;
+        return x.intValue();
     }
 
     public static int find_index(int precision) {
-        BigDecimal desiredGoldenRatio = ONE.add(SQRT_FIVE).divide(TWO, precision, BigDecimal.ROUND_HALF_EVEN);
+        BigDecimal desiredGoldenRatio = ONE.add(SQRT_FIVE).divide(TWO, precision, BigDecimal.ROUND_DOWN);
         BigDecimal actualGoldenRatio;
 
         int i = 3;
@@ -38,11 +36,7 @@ public class Fibonacci {
             BigDecimal a = new BigDecimal(fib(i));
             BigDecimal b = new BigDecimal(fib(i - 1));
 
-            actualGoldenRatio = a.divide(b, precision, BigDecimal.ROUND_HALF_EVEN);
-
-            System.out.println(desiredGoldenRatio);
-            System.out.println(actualGoldenRatio);
-            System.out.println();
+            actualGoldenRatio = a.divide(b, precision, BigDecimal.ROUND_DOWN);
 
             if(actualGoldenRatio.equals(desiredGoldenRatio))
                 return i;
