7f696504f4fdbe0b15a4f2d9ae8499a1e7c8ca3e
Kaarel Kivistik <kaarelkivistik@gmail.com>
2015-09-21 13:59:06 +0300

    binary search

--- AL02/src/Main.java
+++ AL02/src/Main.java
@@ -6,9 +6,12 @@ import NumberGame.GuessMyNumber;
 public class Main {
     public static void main(String[] args) {
         int[] array = {321, 432, 902, 327, 840, 1000, 20390};
+        int[] array2 = {1, 10, 20, 21, 34, 100, 101, 102, 333, 400, 500, 10000, 10200};
 
         GuessMyNumber guessMyNumber = new GuessMyNumber(327);
+        GuessMyNumber guessMyNumber2 = new GuessMyNumber(500);
 
-        System.out.println("Guessed number " + guessMyNumber.playGame(array));
+        System.out.println("Guessed number (1) " + guessMyNumber.playGame(array));
+        System.out.println("Guessed number (2) " + guessMyNumber2.playGame(array2));
     }
 }
--- AL02/src/NumberGame/GuessMyNumber.java
+++ AL02/src/NumberGame/GuessMyNumber.java
@@ -33,13 +33,41 @@ public class GuessMyNumber {
      * @return guessed number
      */
     public int playGame(int[] array) {
-        // TODO: write your code here
+        boolean expectArrayIsSorted = true;
 
-        for(int i = 0; i < array.length; i++) {
-            if(oracle(array[i]) == 0)
+        for(int i = 1; i < Math.min(100, array.length); i++)
+            if(array[i - 1] > array[i]){
+                expectArrayIsSorted = false;
+                break;
+            }
+
+        if(expectArrayIsSorted) {
+            System.out.println("Array is sorted, use binary search.");
+
+            int low = 0;
+            int high = array.length - 1;
+
+            while(high >= low) {
+                int mid = (low + high) / 2;
+                int guess = oracle(array[mid]);
+
+                if(guess == -1) {
+                   high = mid - 1;
+                } else if(guess == 1) {
+                    low = mid + 1;
+                } else {
+                    return array[mid];
+                }
+            }
+        }
+
+        System.out.println("Array is not sorted, use for-loop.");
+
+        for (int i = 0; i < array.length; i++) {
+            if (oracle(array[i]) == 0)
                 return array[i];
         }
-        
+
         return 1;
     }
 }
