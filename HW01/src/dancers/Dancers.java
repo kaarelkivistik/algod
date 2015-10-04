8dbf4a48361b426a1838d3237b0052396281a671
Kaarel Kivistik <kaarelkivistik@gmail.com>
2015-10-05 01:06:11 +0300

    refaktooring

--- HW01/src/dancers/Dancers.java
+++ HW01/src/dancers/Dancers.java
@@ -1,22 +1,234 @@
 package dancers;
 
-import java.util.AbstractMap.SimpleEntry;
+import dancers.Dancer;
+import dancers.IDancer;
+import dancers.IDancers;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.TreeMap;
+import java.util.function.Consumer;
 
+/**
+ * Created by kaarel on 04/10/15.
+ */
 public class Dancers implements IDancers {
+    private Node root;
 
-    TreeMap<DancerKey, Dancer> tree = new TreeMap<>();
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    public Node add(Dancer dancer) {
+        return add(new Node(dancer));
+    }
+
+    public Node add(Node node) {
+        if(root == null)
+            root = node;
+        else
+            add(root, node);
+
+        return node;
+    }
+
+    public Node add(Node root, Node node) {
+        int comparison = node.compareTo(root);
+
+        if(comparison < 0) {
+            if(root.getLeft() == null)
+                root.setLeft(node);
+            else
+                add(root.getLeft(), node);
+        } else {
+            if(root.getRight() == null)
+                root.setRight(node);
+            else
+                add(root.getRight(), node);
+        }
+
+        return node;
+    }
+
+    public Node findPartnerFor(int height, boolean male) {
+        return null;
+    }
+
+    public Node minimum(Node node) {
+        while(node.getLeft() != null)
+            node = node.getLeft();
+
+        return node;
+    }
+
+    public Node maximum(Node node) {
+        while(node.getRight() != null)
+            node = node.getRight();
+
+        return node;
+    }
+
+    public Node successor(Node node) {
+        if(node.getRight() != null)
+            return minimum(node.getRight());
+
+        Node parent = node.getParent();
+
+        while(parent != null && node == parent.getRight()) {
+            node = parent;
+            parent = parent.getParent();
+        }
+
+        return parent;
+    }
+
+    public void delete(Node node) {
+        if(node.getLeft() == null && node.getRight() == null) {
+            replaceSubtree(node, null);
+        } else if(node.getLeft() == null) {
+            replaceSubtree(node, node.getRight());
+        } else if(node.getRight() == null) {
+            replaceSubtree(node, node.getLeft());
+        } else {
+            Node successor = successor(node);
+
+            node.replace(successor);
+            replaceSubtree(successor, successor.getRight());
+        }
+    }
+
+    public void replaceSubtree(Node node, Node replacement) {
+        Node parent = node.getParent();
+
+        if(parent == null) {
+            root = replacement;
+
+        //   if(replacement != null)
+        //        replacement.setParent(null);
+        } else if(node == parent.getLeft())
+            parent.setLeft(replacement);
+        else
+            parent.setRight(replacement);
+
+        if(replacement != null)
+            replacement.setParent(parent);
+    }
+
+    public void traverse(Consumer<Dancer> consumer) {
+        traverse(root, consumer);
+    }
+
+    public void traverse(Node node, Consumer<Dancer> consumer) {
+        if(node == null)
+            return;
+
+        traverse(node.getLeft(), consumer);
+        consumer.accept(node);
+        traverse(node.getRight(), consumer);
+    }
+
+    public List<Dancer> toList() {
+        ArrayList<Dancer> list = new ArrayList<>();
+
+        traverse(list::add);
+
+        return list;
+    }
 
     @Override
-    public SimpleEntry<IDancer, IDancer> findPartnerFor(IDancer d) throws IllegalArgumentException {
+    public AbstractMap.SimpleEntry<IDancer, IDancer> findPartnerFor(IDancer d) throws IllegalArgumentException {
         return null;
     }
 
     @Override
     public List<IDancer> returnWaitingList() {
-        // TODO Auto-generated method stub
         return null;
     }
 
+    public static class Node extends Dancer implements Comparable<Node> {
+
+        private Node left, right, parent;
+
+        public Node(int ID, boolean male, int height) {
+            super(ID, male, height);
+        }
+
+        public Node(Dancer dancer) {
+            this(dancer.getID(), dancer.isMale(), dancer.getHeight());
+        }
+
+        public Node replace(Node node) {
+            setID(node.getID());
+            setMale(node.isMale());
+            setHeight(node.getHeight());
+
+            // node.setParent(getParent());
+            // node.setLeft(getLeft());
+            // node.setRight(getRight());
+
+            return this;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            if(getHeight() < o.getHeight())
+                return -1;
+            else if(getHeight() == o.getHeight()) {
+                if(isMale() == o.isMale())
+                    return 0;
+                else if(isMale())
+                    return -1;
+                else
+                    return 1;
+            } else {
+                return 1;
+            }
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node left) {
+            if(left != null)
+                left.setParent(this);
+
+            this.left = left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public void setRight(Node right) {
+            if(right != null)
+                right.setParent(this);
+
+            this.right = right;
+        }
+
+        public Node getParent() {
+            return parent;
+        }
+
+        public void setParent(Node parent) {
+            this.parent = parent;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "ID=" + getID() +
+                    ", height=" + getHeight() +
+                    ", male=" + isMale() +
+                    ", left=" + left +
+                    ", right=" + right +
+                    "}";
+        }
+    }
 }
+
--- HW01/test/dancers/DancersTest.java
+++ HW01/test/dancers/DancersTest.java
@@ -1,15 +1,17 @@
 package dancers;
 
+import dancers.Dancer;
+import dancers.Dancers;
 import org.junit.Test;
 
-import java.util.AbstractMap.SimpleEntry;
+import java.util.AbstractMap;
 import java.util.Arrays;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
 /**
- * Created by kaarel on 02/10/15.
+ * Created by kaarel on 04/10/15.
  */
 public class DancersTest {
 
@@ -30,7 +32,7 @@ public class DancersTest {
 
         assertThat("there should be a match for female1 (male1)",
                 dancers.findPartnerFor(female1),
-                is(new SimpleEntry<>(male1, female1)));
+                is(new AbstractMap.SimpleEntry<>(female1, male1)));
         assertEquals("list should be empty", dancers.returnWaitingList().size(), 0);
 
         assertNull("there should be no match for male2", dancers.findPartnerFor(male2));
@@ -38,14 +40,147 @@ public class DancersTest {
 
         assertThat("there should be a match for female2 (male3)",
                 dancers.findPartnerFor(female2),
-                is(new SimpleEntry<>(male3, female2)));
+                is(new AbstractMap.SimpleEntry<>(female2, male3)));
         assertThat("there should be a match for female3 (male2)",
                 dancers.findPartnerFor(female3),
-                is(new SimpleEntry<>(male2, female3)));
+                is(new AbstractMap.SimpleEntry<>(female3, male2)));
     }
 
     @Test
     public void testReturnWaitingList() throws Exception {
 
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+        Dancers tree = new Dancers();
+
+        Dancers.Node node1 = new Dancers.Node(1, true, 160);
+        Dancers.Node node2 = new Dancers.Node(2, true, 165);
+        Dancers.Node node3 = new Dancers.Node(3, true, 165);
+        Dancers.Node node4 = new Dancers.Node(4, true, 163);
+        Dancers.Node node5 = new Dancers.Node(5, false, 163);
+        Dancers.Node node6 = new Dancers.Node(6, false, 170);
+
+        tree.add(node1);
+        tree.add(node2);
+        tree.add(node3);
+        tree.add(node4);
+        tree.add(node5);
+        tree.add(node6);
+
+        Dancers.Node expectedRootNode = new Dancers.Node(1, true, 160);
+        expectedRootNode.setRight(new Dancers.Node(2, true, 165));
+        expectedRootNode.getRight().setRight(new Dancers.Node(3, true, 165));
+        expectedRootNode.getRight().setLeft(new Dancers.Node(4, true, 163));
+        expectedRootNode.getRight().getLeft().setRight(new Dancers.Node(5, false, 163));
+        expectedRootNode.getRight().getRight().setRight(new Dancers.Node(6, false, 170));
+
+        assertEquals(expectedRootNode.toString(), tree.getRoot().toString());
+
+        assertEquals("successor to 160 is 163", node4, tree.successor(node1));
+        assertEquals("successor to 163 is 165", node3, tree.successor(node2));
+        assertEquals("successor to 165 is 170", node6, tree.successor(node3));
+        assertEquals("successor to 170 is null", null, tree.successor(node6));
+
+        assertThat(tree.toList(),
+                is(Arrays.asList(
+                        node1,
+                        node4,
+                        node5,
+                        node2,
+                        node3,
+                        node6
+                )));
+    }
+
+    @Test
+    public void testReplace() throws Exception {
+        Dancers.Node original = new Dancers.Node(0, false, 1);
+        Dancers.Node parent = new Dancers.Node(10, true, 20);
+        Dancers.Node child = new Dancers.Node(20, true, 30);
+        Dancers.Node replacement = new Dancers.Node(2, true, 3);
+
+        original.setParent(parent);
+        original.setLeft(child);
+        original.replace(replacement);
+
+        assertEquals(original, replacement);
+        assertEquals(original.getParent(), parent);
+        assertEquals(original.getLeft(), child);
+    }
+
+    @Test
+    public void testReplaceSubtree() throws Exception {
+        Dancers tree = new Dancers();
+
+        Dancers.Node node1 = new Dancers.Node(1, true, 160);
+        Dancers.Node node2 = new Dancers.Node(2, true, 165);
+        Dancers.Node node3 = new Dancers.Node(3, true, 165);
+        Dancers.Node node4 = new Dancers.Node(4, true, 163);
+        Dancers.Node node5 = new Dancers.Node(5, false, 163);
+        Dancers.Node node6 = new Dancers.Node(6, false, 170);
+
+        tree.add(node1);
+        tree.add(node2);
+        tree.add(node3);
+        tree.add(node4);
+        tree.add(node5);
+        tree.add(node6);
+
+        tree.replaceSubtree(node2, node4);
+
+        assertEquals(node1.getRight(), node4);
+    }
+
+    @Test public void testDelete() throws Exception {
+        Dancers tree = new Dancers();
+
+        Dancers.Node node1 = new Dancers.Node(1, true, 160);
+        Dancers.Node node2 = new Dancers.Node(2, true, 165);
+        Dancers.Node node3 = new Dancers.Node(3, true, 165);
+        Dancers.Node node4 = new Dancers.Node(4, true, 163);
+        Dancers.Node node5 = new Dancers.Node(5, false, 163);
+        Dancers.Node node6 = new Dancers.Node(6, false, 170);
+
+        tree.add(node1);
+        tree.add(node2);
+        tree.add(node3);
+        tree.add(node4);
+        tree.add(node5);
+        tree.add(node6);
+
+        tree.delete(node6);
+
+        assertNull(node3.getRight());
+
+        tree.delete(node4);
+
+        assertEquals(node5, node2.getLeft());
+
+        tree.delete(node2);
+
+        assertEquals(node2, node3);
+    }
+
+    @Test
+    public void testDeleteRoot() throws Exception {
+        Dancers tree = new Dancers();
+
+        Dancers.Node node1 = new Dancers.Node(1, true, 2);
+        Dancers.Node node2 = new Dancers.Node(1, true, 1);
+        Dancers.Node node3 = new Dancers.Node(1, true, 3);
+        Dancers.Node node4 = new Dancers.Node(1, true, 4);
+
+        tree.add(node1);
+        tree.add(node2);
+        tree.add(node3);
+        tree.add(node4);
+
+        tree.delete(node1);
+
+        assertEquals(node2, tree.getRoot().getLeft());
+        assertEquals(node3, tree.getRoot());
+        assertEquals(node4, tree.getRoot().getRight());
     }
 }
