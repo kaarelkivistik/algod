56e63fb89b3e766049481715a50c89cef0c08505
Kaarel Kivistik <kaarelkivistik@gmail.com>
2015-11-02 17:05:11 +0200

    viies praks

---
+++ AL05/test/greedytsp/greedyTSPTest.java
@@ -0,0 +1,44 @@
+package greedytsp;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kaarel on 02/11/15.
+ */
+public class greedyTSPTest {
+
+    @org.junit.Test
+    public void testGreedySolution() throws Exception {
+        int[][] adjacencyMatrix = new int[][] {
+                {0, 1, 4, 7, 8},
+                {3, 0, 9, 6, 2},
+                {2, 4, 0, 5, 3},
+                {8, 5, 9, 0, 1},
+                {9, 6, 7, 3, 0}
+        };
+
+        greedyTSP.greedySolution(adjacencyMatrix);
+    }
+
+    @org.junit.Test
+    public void testFindSmallestNotVisited() throws Exception {
+        HashSet<Integer> visited = new HashSet<>();
+
+        visited.add(0);
+        visited.add(2);
+
+        assertEquals(1, greedyTSP.findSmallestNotVisited(new int[]{10, 9, 0, 103, 94}, visited));
+
+        visited.clear();
+        visited.add(0);
+        visited.add(4);
+
+        assertEquals(5, greedyTSP.findSmallestNotVisited(new int[]{0, 900, 222, 439, 19, 28}, visited));
+    }
+}
