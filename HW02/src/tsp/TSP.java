a4a1c18ab5557bf4866996560f5277e5a7096cfe
Kaarel Kivistik <kaarel@KaarelMbp15.local>
2015-11-16 04:34:50 +0200

    valmis

--- HW02/src/tsp/TSP.java
+++ HW02/src/tsp/TSP.java
@@ -15,4 +15,19 @@ public class TSP {
 
 		return myTSP.findOptimalTourUsingBfs().stream().mapToInt(i -> i).toArray();
 	}
+
+    /*
+        Sügavuti otsingu ajad:
+            testDfsSmall took 1 milliseconds
+            testDfsSmallWithoutBNB took 0 milliseconds
+            testDfsMedium took 89 milliseconds
+            testDfsMediumWithoutBNB took 32579 milliseconds
+            testDfsBig took 4898 milliseconds
+            ("big" maatriksit ei hakanud mõõtma, arvestades "keskmisele" kulunud aega)
+
+        Parim-enne otsingu ajad:
+            testBfsSmall took 0 milliseconds
+            testBfsMedium took 198 milliseconds
+            testBfsBig took 4097 milliseconds
+     */
 }
--- HW02/test/tsp/TSPTest.java
+++ HW02/test/tsp/TSPTest.java
@@ -12,65 +12,113 @@ public class TSPTest {
 
     @Test
     public void testBfsSmall() throws Exception {
+        long startTime = System.currentTimeMillis();
+
         MyTSP myTSP = new MyTSP(TestData.smallMatrix);
 
         assertEquals("Result distance should be 405",
                 405, myTSP.length(myTSP.findOptimalTourUsingBfs()));
+
+        long duration = System.currentTimeMillis() - startTime;
+
+        System.out.println("testBfsSmall took " + duration + " milliseconds");
     }
 
     @Test
     public void testBfsMedium() throws Exception {
+        long startTime = System.currentTimeMillis();
+
         MyTSP myTSP = new MyTSP(TestData.mediumMatrix);
 
         assertEquals("Result distance should be 772",
                 772, myTSP.length(myTSP.findOptimalTourUsingBfs()));
+
+        long duration = System.currentTimeMillis() - startTime;
+
+        System.out.println("testBfsMedium took " + duration + " milliseconds");
     }
 
     @Test
     public void testBfsBig() throws Exception {
+        long startTime = System.currentTimeMillis();
+
         MyTSP myTSP = new MyTSP(TestData.bigMatrix);
 
         assertEquals("Result distance should be 893",
                 893, myTSP.length(myTSP.findOptimalTourUsingBfs()));
+
+        long duration = System.currentTimeMillis() - startTime;
+
+        System.out.println("testBfsBig took " + duration + " milliseconds");
     }
 
     @Test
     public void testDfsSmall() throws Exception {
+        long startTime = System.currentTimeMillis();
+
         MyTSP myTSP = new MyTSP(TestData.smallMatrix);
 
         assertEquals("Result distance should be 405",
                 405, myTSP.length(myTSP.findOptimalTourUsingDfs(false)));
+
+        long duration = System.currentTimeMillis() - startTime;
+
+        System.out.println("testDfsSmall took " + duration + " milliseconds");
     }
 
     @Test
     public void testDfsSmallWithoutBNB() throws Exception {
+        long startTime = System.currentTimeMillis();
+
         MyTSP myTSP = new MyTSP(TestData.smallMatrix);
 
         assertEquals("Result distance should be 405",
                 405, myTSP.length(myTSP.findOptimalTourUsingDfs(true)));
+
+        long duration = System.currentTimeMillis() - startTime;
+
+        System.out.println("testDfsSmallWithoutBNB took " + duration + " milliseconds");
     }
 
     @Test
     public void testDfsMedium() throws Exception {
+        long startTime = System.currentTimeMillis();
+
         MyTSP myTSP = new MyTSP(TestData.mediumMatrix);
 
         assertEquals("Result distance should be 772",
                 772, myTSP.length(myTSP.findOptimalTourUsingDfs(false)));
+
+        long duration = System.currentTimeMillis() - startTime;
+
+        System.out.println("testDfsMedium took " + duration + " milliseconds");
     }
 
     @Test
     public void testDfsMediumWithoutBNB() throws Exception {
+        long startTime = System.currentTimeMillis();
+
         MyTSP myTSP = new MyTSP(TestData.mediumMatrix);
 
         assertEquals("Result distance should be 772",
                 772, myTSP.length(myTSP.findOptimalTourUsingDfs(true)));
+
+        long duration = System.currentTimeMillis() - startTime;
+
+        System.out.println("testDfsMediumWithoutBNB took " + duration + " milliseconds");
     }
 
     @Test
     public void testDfsBig() throws Exception {
+        long startTime = System.currentTimeMillis();
+
         MyTSP myTSP = new MyTSP(TestData.bigMatrix);
 
         assertEquals("Result distance should be 893",
                 893, myTSP.length(myTSP.findOptimalTourUsingDfs(false)));
+
+        long duration = System.currentTimeMillis() - startTime;
+
+        System.out.println("testDfsBig took " + duration + " milliseconds");
     }
 }
